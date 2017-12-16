8e68ae32ea6fb2aad34aa5f973565e2cee5c5983
Ryo Ota <nwtgck@gmail.com>
2017-12-16 12:21:35 +0900

    [change] Extract the example to a function

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,15 @@ module Main where
 
 import Data.String.Interpolate
 
-main :: IO ()
-main = do
+prac1 :: IO ()
+prac1 = do
+  putStrLn "===== Practice 1 ====="
   let name = "Jack"
       age  = 23
   putStrLn [i| Name is #{name}. His age is #{age}.|]
+  putStrLn [i| Name is #{name}. His age will be #{age+1} next year.|]
+
+
+main :: IO ()
+main = do
+  prac1
