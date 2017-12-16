9d1984d2bff98e46f64bff03fbeba568f78ac7ed
Ryo Ota <nwtgck@gmail.com>
2017-12-16 12:24:27 +0900

    [add] Add example to use a function in '#{}'

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,14 @@ prac1 = do
   putStrLn [i| Name is #{name}. His age is #{age}.|]
   putStrLn [i| Name is #{name}. His age will be #{age+1} next year.|]
 
+prac2 :: IO ()
+prac2 = do
+  putStrLn "===== Practice 2 using a function in '#{}' ====="
+  let list = [83, 1839, 12, -934, 78, 9, 0]
+  putStrLn [i| The max value in #{list} is #{maximum list}.|]
+
 
 main :: IO ()
 main = do
   prac1
+  prac2
